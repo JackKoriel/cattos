590f1e68ce86f2e417a9fb25b05b15ac7eedcb26
jkoriel <jkoriel@gmail.com>
2026-02-15 23:44:04 -0500

    feat: adds a theme fallback for tests

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,8 +2,10 @@ export * from './components'
 export * from './theme'
 export { ThemeProvider } from '@mui/material'
 
-import { useTheme } from '@mui/material/styles'
+import { useTheme, createTheme } from '@mui/material/styles'
 import type { Theme } from './theme'
 
-export const useAppTheme = () => useTheme<Theme>()
+const _defaultTheme = createTheme()
+
+export const useAppTheme = () => useTheme<Theme>() ?? (_defaultTheme as Theme)
 export type { Theme as AppTheme } from './theme'
